4d51b65602988e28e6b88bbed28bbc7748804ce8
Saidazim <134857217+saidazimisroil@users.noreply.github.com>
2024-08-14 04:38:36 +0500

    add fetching data from database

--- database/migrations/2024_08_13_221918_create_tasks_table.php
+++ database/migrations/2024_08_13_221918_create_tasks_table.php
@@ -14,9 +14,10 @@ return new class extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
 
-            $table->string('titles');
+            $table->string('title');
             $table->text('description');
             $table->text('long_description')->nullable();
+            $table->boolean('completed');
 
             $table->timestamps();
         });
