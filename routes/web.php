8a1a224748ef74c33590b06a80da09c6db95751a
Saidazim <134857217+saidazimisroil@users.noreply.github.com>
2024-08-14 20:46:29 +0500

    add update task

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,12 @@ Route::get('/tasks', function () {
 Route::view('/tasks/create', 'create')
   ->name('tasks.create');
 
+Route::get('/tasks/{id}/edit', function ($id) {
+  return view('edit', [
+      'task' => Task::findOrFail($id)
+  ]);
+})->name('tasks.edit');
+
 Route::get('/tasks/{id}', function ($id) {
   return view('show', [
       'task' => Task::findOrFail($id)
@@ -41,3 +47,22 @@ Route::post('/tasks', function (Request $request) {
   return redirect()->route('tasks.show', ['id' => $task->id])
     ->with('success', 'Task has been created successfully');
 })->name('tasks.store');
+
+Route::put('/tasks/{id}', function ($id, Request $request) {
+  $data = $request->validate([
+    'title' => 'required|max:255',
+    'description' => 'required',
+    'long_description' => 'required',
+  ]);
+
+  $task = new Task;
+  $task->title = $data['title'];
+  $task->description = $data['description'];
+  $task->long_description = $data['long_description'];
+  $task->completed = false;
+
+  $task->save();
+
+  return redirect()->route('tasks.show', ['id' => $task->id])
+    ->with('success', 'Task has been updated successfully');
+})->name('tasks.update');
