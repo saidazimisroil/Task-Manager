d4b5e36321a2a597b802984c0ca3db998f135e81
Saidazim <134857217+saidazimisroil@users.noreply.github.com>
2024-08-15 20:54:54 +0500

    add tailwind-css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,18 +3,23 @@
 @section('title', 'List of Tasks')
     
 @section('content')
-  <a href="{{ route('tasks.create') }}">Add Task</a>
+  <nav class="mb-4">
+    <a href="{{ route('tasks.create') }}" class="font-medium text-gray-700 underline decoration-green-500">Add Task</a>
+  </nav>
 
   <div>
     @forelse ($tasks as $task)
-      <li><a href="{{ route('tasks.show', ['task' => $task]) }}">{{ $task->title }}</a></li>
+      <li>
+        <a href="{{ route('tasks.show', ['task' => $task]) }}"
+          @class(['line-through' => $task->completed])>{{ $task->title }}</a>
+      </li>
     @empty
       <h3>There is no any task</h3>
     @endforelse
   </div>
 
   @if ($tasks->count())
-    <nav>
+    <nav class="mt-4">
       {{ $tasks->links() }}
     </nav>
   @endif
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,10 +4,11 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <script src="https://cdn.tailwindcss.com"></script>
   <title>Task Manager</title>
 </head>
-<body>
-  <h1>@yield('title')</h1>
+<body class="container mx-auto my-10 max-w-lg">
+  <h1 class="text-2xl mb-4">@yield('title')</h1>
 
   @if ( session()->has('success'))
     <p><b>{{ session('success') }}</b></p>
